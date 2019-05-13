b0b49cbbea9e5eca0650b57510f7a7b1acd02fdd
Franchely Portorreal <franchely811@gmail.com>
2019-05-13 21:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ function total() {
 function removeFromCart(item) {
   for(var i = 0, l = cart.length; i < l; i++){
     for(var list in cart[i]){
-      console.log(cart)
         if(item === list){
           cart.splice(i,1)
           return cart
