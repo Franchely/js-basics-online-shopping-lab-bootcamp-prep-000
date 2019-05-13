bc36c1bb6762ad6f82787a579a4c2e828298b70c
Franchely Portorreal <franchely811@gmail.com>
2019-05-13 20:53:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    return "Sorry, we don't have a credit card on file for you."
+     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   } else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   return "Sorry, we don't have a credit card on file for you."
   }
 }
 
