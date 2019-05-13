7941f78334c3fe7f0c25d201ad8d995693e6e2f6
Franchely Portorreal <franchely811@gmail.com>
2019-05-13 20:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
 }  
 
 function placeOrder(cardNumber) {
-  if (cardNumber)isNaN; {
+  if (isNan(cardNumber)) {
     return "Sorry, we don't have a credit card on file for you."
   }  {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
